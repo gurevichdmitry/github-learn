4c03ee19f06e5459fc8ce9b3258605cfeee45dc7
Dmitry Gurevich <99176494+gurevichdmitry@users.noreply.github.com>
2023-11-22 16:51:16 +0200

    add script

--- .github/scripts/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Dzed Baradzed"
-expr 1 + 1 > output.log
---
+++ .github/scripts/get-released-version.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Input: version to calculate previous version
+VERSION="$1"
+
+# Extract the major and minor versions
+MAJOR_VERSION=$(echo $VERSION | cut -d'.' -f1)
+MINOR_VERSION=$(echo $VERSION | cut -d'.' -f2)
+
+# Calculate the previous version (assuming it's always X.(Y-1))
+PREVIOUS_VERSION="$MAJOR_VERSION.$((MINOR_VERSION - 1))"
+
+URL="https://snapshots.elastic.co/latest/$PREVIOUS_VERSION.json"
+
+# Use curl to fetch the JSON data
+JSON_RESPONSE=$(curl -s "$URL")
+
+# Get latest snapshot version
+SNAPSHOT_VERSION=$(echo "$JSON_RESPONSE" | jq -r '.version')
+
+# Split the version into major, minor, and patch parts
+IFS='.-' read -ra PARTS <<< "$SNAPSHOT_VERSION"
+MAJOR="${PARTS[0]}"
+MINOR="${PARTS[1]}"
+PATCH="${PARTS[2]}"
+
+# Decrement the patch version by 1
+PATCH=$((PATCH - 1))
+
+# Format the previous version
+PREVIOUS_VERSION="$MAJOR.$MINOR.$PATCH"
+
+# echo "Current Version: $VERSION"
+# echo "Latest Released Version: $PREVIOUS_VERSION"
+# Output the previous version
+echo "$PREVIOUS_VERSION"
